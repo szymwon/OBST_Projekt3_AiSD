39cf1698f5a12744d5576b98c163f10e2553a85e
szymwon <onexz.kontakt@gmail.com>
2026-05-29 13:52:38 +0200

    stworzenie funkcji ktora laduje plik do slownika

--- search optimization.py
+++ search optimization.py
@@ -1,32 +1,22 @@
 # -*- coding: utf-8 -*-
 
 def loadFile(fileName):
-    """"""
+    """Loading items file to a dictionary."""
     itemsDict ={}
     # file = open(nazwa_pliku,"r")3
     try:
         with open(fileName, "r") as file:
             for line in file:
-                line.strip()
+                line = line.strip()
                 if line == "": #bullet-proofing for empty lines
                     continue
                 separated_line = line.split(",")
-                itemsDict[separated_line[0]] = separated_line[1]
+                itemsDict[separated_line[0]] = int(separated_line[1].strip())
+        return itemsDict
     except Exception:
-        print(f"Błąd wczytania pliku {fileName}")
+        print(f"Błąd wczytania pliku \"{fileName}\"")
         raise
 
 
 
-#
-# with open("items.csv", "r") as file:
-#     items = file.
-
-# items_dic = {}
-
-
-# for item,freq in items:
-
-
-
-print(items)
+dict = loadFile("items.csv")
